be219d3bafeb8e8509dd50cd8339e19a8173982e
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:28:47 +0200

    Add comments

--- Solver.h
+++ Solver.h
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+/**
+ * Solver Class
+ *
+ * This class is a generic interface which get a problem a and return a solution.
+ * The class that will implements this interface knows to solve a given problem.
+ * It also knows to return a deep copy of this solver and get the solver name.
+ *
+ * @param Problem that we want to solve.
+ * @param Solution we solved for a given problem.
+ */
 template <class Problem, class Solution>
 class Solver {
 protected:
@@ -12,10 +22,15 @@ protected:
 public:
     virtual ~Solver() {}
     virtual Solution solve(Problem problem) = 0;
+    virtual Solver<Problem,Solution>* deepCopy() = 0;
+
+    /**
+     * This function return a string of this solver name.
+     * @return this solver name.
+     */
     virtual string getName() {
         return this->solverName;
     }
-    virtual Solver<Problem,Solution>* deepCopy() = 0;
 };
 
 #endif //PROBLEM_SOLVING_SOLVER_H
