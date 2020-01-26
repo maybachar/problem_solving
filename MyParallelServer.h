81c6372ad1894b2afeedf1bdb77305699b7cf961
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:19:57 +0200

    Add comments

--- MyParallelServer.h
+++ MyParallelServer.h
@@ -5,14 +5,24 @@
 #include <thread>
 #include <vector>
 #include "Server.h"
+#include "ClientHandler.h"
 
 using namespace std;
 
+/**
+ * MyParallelServer Class
+ *
+ * This class implements the Server interface.
+ * This class create a server which treats clients in a parallel form.
+ * It open a server which open for listening in separate thread, it accept
+ * clients so each client is treated by a new client handler in a new thread.
+ * So, we can accept and handle clients in parallel form. The server
+ * will stop listening after 2 minutes that no client is connected.
+ */
 class MyParallelServer : public server_side::Server {
     bool shouldStop = false;
     int socketfd;
     thread serverThread;
-    vector<thread> vecThread;
 public:
     MyParallelServer();
     virtual void open(int port, ClientHandler* handler);
