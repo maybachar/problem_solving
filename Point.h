070fa4643d9effae87611d9b4df6c25ef65d2392
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:24:24 +0200

    Add comments

--- Point.h
+++ Point.h
@@ -1,6 +1,15 @@
 #ifndef PROBLEM_SOLVING_POINT_H
 #define PROBLEM_SOLVING_POINT_H
 
+using namespace std;
+
+/**
+ * Point Class
+ *
+ * This class representing a point which hold x and y value-
+ * the point's location. This class knows to get the x and y values
+ * and compare if a given point is equals to this point.
+ */
 class Point {
     int xPos;
     int yPos;
