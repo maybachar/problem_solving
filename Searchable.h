8220dbd50726aad68a0ce6f296294c044da147b5
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:26:26 +0200

    Add comments

--- Searchable.h
+++ Searchable.h
@@ -6,10 +6,18 @@
 
 using namespace std;
 
+/**
+ * Searchable Class
+ *
+ * This class is a generic interface representing a problem we want to solve.
+ * It knows to get the initial state-the source and also to get the goal state-the target.
+ * It knows to return true if a given state is the goal state and false if isn't.
+ * It also knows to get a vector of all the possible state of a given state.
+ */
 template <class T>
 class Searchable {
 public:
-    ~Searchable() {}
+    virtual ~Searchable() {}
     virtual State<T>* getInitialState() = 0;
     virtual State<T>* getGoalState() = 0;
     virtual bool isGoalState(State<T>*) = 0;
