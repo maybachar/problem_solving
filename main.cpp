b5e80c52ace9b19ddfa2e9de41da43dc8cee21a5
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:44:49 +0200

    Add comments

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 #include "SearchSolver.h"
 #include "Searchable.h"
 #include "Point.h"
-#include "BFS.h"
-#include "DFS.h"
-#include "BestFirstSearch.h"
 #include "AStar.h"
 #include "MyParallelServer.h"
 #include <string>
@@ -16,14 +13,42 @@
 
 using namespace std;
 
+/**
+ * Main class
+ *
+ * This class contain functions which run the program in different way,
+ * for different problems, different server form, different client handler
+ * and different solution.
+ * we will call in the major main to the suitable function in this class
+ * according to the problem we want to solve and run.
+ */
 namespace boot {
     class Main {
     public:
+        /**
+         * This function running the program for the string reverse problem.
+         * The server treats the clients in a serial form,
+         * and send the suitable solution -reverse string, to each client.
+         *
+         * @param argc number of command line arguments.
+         * @param argv command line arguments.
+         */
         void stringReverseMain(int argc, char *argv[]) {
-            int port = atoi(argv[1]);
+            (void) argc;
+            int port;
+            if (argc < 2) {
+                port = 5600;
+            } else {
+                // Get a port as argument
+                port = atoi(argv[1]);
+            }
+            // Create a solver for the problem
             Solver<string, string>* sr = new StringReverser();
+            // Create a cache manager
             CacheManager<string, string>* cm = new FileCacheManager<string, string>();
+            // Create a suitable client handler for this string reversing problem
             ClientHandler* handler = new MyTestClientHandler<string, string>(sr, cm);
+            // Create a server which treat clients in a serial form
             server_side::Server* server = new MySerialServer();
             server->open(port, handler);
             delete sr;
@@ -32,16 +57,28 @@ namespace boot {
             delete server;
         }
 
+        /**
+         * This function running the program for the matrix problem.
+         * The server treats the clients in a parallel form,
+         * and send the suitable solution, the shortest path of lowest cost path,
+         * to each client.
+         *
+         * @param argc number of command line arguments.
+         * @param argv command line arguments.
+         */
         void matrixMain(int argc, char *argv[]) {
-            int port = atoi(argv[1]);
-            //Searcher<Point*, vector<State<Point*>*>>* searcher = new BFS<Point*, vector<State<Point*>*>>();
-            //Searcher<Point*, vector<State<Point*>*>>* searcher = new DFS<Point*, vector<State<Point*>*>>();
+            (void) argc;
+            int port;
+            if (argc < 2) {
+                port = 5600;
+            } else {
+                // Get a port as argument
+                port = atoi(argv[1]);
+            }
             Searcher<Point*, vector<State<Point*>*>>* searcher = new AStar<Point*, vector<State<Point*>*>>();
-            //Searcher<Point*, vector<State<Point*>*>>* searcher = new BestFirstSearch<Point*, vector<State<Point*>*>>();
             Solver<Searchable<Point*>*, string>* sr = new SearchSolver(searcher);
             CacheManager<string, string>* cm = new FileCacheManager<string, string>();
             ClientHandler* handler = new MyClientHandler<Searchable<Point*>*, string>(sr, cm);
-            //server_side::Server* server = new MySerialServer();
             server_side::Server* server = new MyParallelServer();
             server->open(port, handler);
             delete searcher;
@@ -50,13 +87,19 @@ namespace boot {
             delete handler;
             delete server;
         }
-
     };
 }
 
-
+/**
+ * This function is responsible for running the program.
+ *
+ * @param argc number of command line arguments.
+ * @param argv command line arguments.
+ * @return 0
+ */
 int main(int argc, char *argv[]) {
-    boot::Main* main = new boot::Main;
+    boot::Main* main;
+    // Run the matrix main in class Main
     main->matrixMain(argc, argv);
     return 0;
 }
