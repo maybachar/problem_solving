09f28834674dd3de305012584b3bb49db73f87bd
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:28:11 +0200

    Add comments

--- Server.h
+++ Server.h
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+/**
+ * Server Class
+ *
+ * This class is a server interface.
+ * It knows to open a server which will listen to a given port
+ * and will accept and handle clients in serial or parallel form.
+ * The server will stop listening when time has passed and no client
+ * has been connected for a time that is set(in our case 2 minutes).
+ */
 namespace server_side {
     class Server {
     public:
@@ -15,3 +24,4 @@ namespace server_side {
 }
 
 #endif //PROBLEM_SOLVING_SERVER_H
+
