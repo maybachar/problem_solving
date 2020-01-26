50e3ab791c603c7497a7b2d96b2eb99161fbad1c
ayelet97 <59517104+ayelet97@users.noreply.github.com>
2020-01-26 21:22:33 +0200

    Add comments

--- MySerialServer.h
+++ MySerialServer.h
@@ -1,11 +1,23 @@
 #ifndef PROBLEM_SOLVING_MYSERIALSERVER_H
 #define PROBLEM_SOLVING_MYSERIALSERVER_H
+
 #include <netinet/in.h>
 #include <thread>
 #include "Server.h"
+#include "ClientHandler.h"
 
 using namespace std;
 
+/**
+ * MySerialServer Class
+ *
+ * This class implements the Server interface.
+ * This class create a server which treats clients in a serial form.
+ * It open a server which open for listening in separate thread, it accept a client
+ * that connected to it, handle him and then it is available for accepting another
+ * client and so on- in a serial form. The server will stop listening after 2 minutes
+ * that no client is connected.
+ */
 class MySerialServer : public server_side::Server {
     bool shouldStop = false;
     int socketfd;
